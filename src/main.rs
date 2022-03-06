775a6ee9eedef7a322133b1616e825fab7f2766c
Steven Pham <steven.pham.8.2000@gmail.com>
2022-03-06 03:44:46 -0500

    Add DB models

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use rocket_dyn_templates::{tera::Context, Template};
-use serde::Serialize;
-
 #[macro_use]
 extern crate rocket;
+
+#[macro_use]
+extern crate diesel;
+
+use rocket_dyn_templates::Template;
+
+pub mod models;
+pub mod schema;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -12,6 +17,6 @@ fn index() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, show])
+        .mount("/", routes![index])
         .attach(Template::fairing())
 }
---
+++ src/models.rs
@@ -0,0 +1,45 @@
+use crate::schema::*;
+
+#[derive(Queryable, Identifiable, PartialEq, Debug)]
+pub struct ResearchField {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Queryable, Identifiable, PartialEq, Debug)]
+pub struct Professor {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Queryable, Identifiable, Associations, PartialEq, Debug)]
+#[primary_key(prof_id, field_id)]
+#[belongs_to(ResearchField, foreign_key = "field_id")]
+#[belongs_to(Professor, foreign_key = "prof_id")]
+pub struct ProfessorResearchField {
+    pub prof_id: i32,
+    pub field_id: i32,
+}
+
+#[derive(Queryable, Identifiable, Associations, PartialEq, Debug)]
+#[belongs_to(ResearchField, foreign_key = "desired_field_id")]
+pub struct Applicant {
+    pub id: i32,
+    pub name: String,
+    pub desired_field_id: i32,
+    pub phone_number: String,
+    pub email: String,
+    pub cv_path: String,
+    pub diploma_path: String,
+    pub grade_audit_path: String,
+}
+
+#[derive(Queryable, Identifiable, Associations, PartialEq, Debug)]
+#[primary_key(applicant_id, prof_id)]
+#[belongs_to(Applicant, foreign_key = "applicant_id")]
+#[belongs_to(Professor, foreign_key = "prof_id")]
+#[table_name = "student_applied_to"]
+pub struct StudentAppliedTo {
+    pub applicant_id: i32,
+    pub prof_id: i32,
+}
