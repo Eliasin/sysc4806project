42b9fa168cf94264f419deb2e1ce7201b5c82aee
Kaue Oliveira <elmovckaue@gmail.com>
2022-03-08 16:55:55 -0500

    Define initial routes and add sass rocket fairing

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,42 @@ extern crate rocket;
 extern crate diesel;
 
 use rocket_dyn_templates::Template;
+use sass_rocket_fairing::SassFairing;
+use rocket_dyn_templates::tera::Context;
+use db::DbConn;
 
 pub mod db;
 pub mod models;
 pub mod rest;
 pub mod schema;
 
-use db::DbConn;
+
+#[get("/")]
+fn index() -> Template {
+    let context = Context::new();
+    Template::render("index", &context.into_json())
+}
+
+#[get("/prof")]
+fn prof() -> Template {
+    let context = Context::new();
+    Template::render("prof-select", &context.into_json())
+}
+
+#[get("/student")]
+fn student() -> Template {
+    let context = Context::new();
+    Template::render("student-select", &context.into_json())
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/rest", rest::routes())
+        .mount("/", routes![index, prof, student])
         .attach(Template::fairing())
         .attach(DbConn::fairing())
+        .attach(SassFairing)
+        // .mount("/", routes![index])
+        // .mount("/styles", FileServer::from(relative!("templates/styles")))
 }
