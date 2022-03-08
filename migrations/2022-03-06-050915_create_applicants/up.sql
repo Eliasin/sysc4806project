ecdf2642e296228e205547385fb220cf15e09141
Callum Coughlan <callum.d.w.coughlan@gmail.com>
2022-03-07 20:45:10 -0500

    Make all columns non-null

--- migrations/2022-03-06-050915_create_applicants/up.sql
+++ migrations/2022-03-06-050915_create_applicants/up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE applicants (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    desired_field_id INTEGER REFERENCES research_fields,
-    name text,
-    phone_number text,
-    email text,
-    cv_path text,
-    diploma_path text,
-    grade_audit_path text
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    desired_field_id INTEGER REFERENCES research_fields NOT NULL,
+    name TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    email TEXT NOT NULL,
+    cv_path TEXT NOT NULL,
+    diploma_path TEXT NOT NULL,
+    grade_audit_path TEXT NOT NULL
 );
