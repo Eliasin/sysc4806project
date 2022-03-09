a0171accd3f69c4ad91a3353c46091a426ce7d05
Steven Pham <steven.pham.8.2000@gmail.com>
2022-03-09 01:36:13 -0500

    Test minimal rocket setup

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,20 @@ pub mod models;
 pub mod rest;
 pub mod schema;
 
+#[get("/")]
+pub async fn index() -> &'static str {
+    "Hello World!"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/rest", rest::routes())
-        .mount("/", html::routes())
-        .attach(Template::fairing())
-        .attach(DbConn::fairing())
+    rocket::build().mount("/", routes![index])
+    /*
+    .mount("/rest", rest::routes())
+    .mount("/", html::routes())
+    .attach(Template::fairing())
+    .attach(DbConn::fairing())
+    */
     // .mount("/", routes![index])
     // .mount("/styles", FileServer::from(relative!("templates/styles")))
 }
